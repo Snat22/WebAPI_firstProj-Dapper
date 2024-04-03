82ad3801ff643529edca6350eed0915b4f77969c
Sins <142435643+Snat22@users.noreply.github.com>
2024-04-03 15:39:22 +0500

    Update Company.cs

--- WebApp/Models/Company.cs
+++ WebApp/Models/Company.cs
@@ -4,6 +4,5 @@ public class Company
 {
     public int Id { get; set; }
     public required string Name  { get; set; }
-    public int Department_Id { get; set; }
     public string Address { get; set; }
 }
