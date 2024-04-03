cc644050d26c065d347d07b7f6e5a48480e0c179
Sins <142435643+Snat22@users.noreply.github.com>
2024-04-03 15:45:30 +0500

    Update WebAPI_db.sql

--- WebAPI_db.sql
+++ WebAPI_db.sql
@@ -6,21 +6,21 @@ create table employees
 	age int,
 	phone varchar(50) not null unique,
 	address varchar(100),
-	email varchar(100) unique check(email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+	email varchar(100) unique check(email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+	department_id int references departments(id)
 )
 
 create table departments
 (
 	id serial primary key,
 	name varchar(50) not null,
-	employee_id int references employees(id)
+	company_id int references companyes(id)
 )
 
 create table companyes
 (
 	id serial primary key,
 	name varchar(50) not null,
-	department_id int references departments(id),
 	address varchar(100)
 )
 select * from employees
